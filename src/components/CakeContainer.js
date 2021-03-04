e568c036e49fa301b3a5ea88357f477f06cec01a
Dulatto <dulat.omarov@gmail.com>
2021-03-03 22:30:12 -0500

    redux

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { buyCake } from '../redux'
 
-function CakeContainer() {
+function CakeContainer(props) {
     return (
         <div>
-           <h2>Number of cakes</h2> 
-           <button>Buy Cake</button>
+           <h2>Number of cakes - {props.numOfCakes}</h2> 
+           <button onClick={props.buyCake}>Buy Cake</button>
         </div>
     )
 }
@@ -22,4 +23,6 @@ const mapDispatchToProps = dispatch =>{
     }
 }
 
-export default CakeContainer
+export default connect(mapStateToProps,
+     mapDispatchToProps
+     ) (CakeContainer)
