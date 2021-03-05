5c30b5831fb88eb8ea8c32a9e42214346b28ea42
Dulatto <dulat.omarov@gmail.com>
2021-03-04 23:52:48 -0500

    redux

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -6,7 +6,7 @@ function CakeContainer(props) {
     return (
         <div>
            <h2>Number of cakes - {props.numOfCakes}</h2> 
-           <button onClick={props.buyCake}> Buy Cake </button>
+           <button onClick={props.buyCake}> Buy Cake</button>
         </div>
     )
 }
