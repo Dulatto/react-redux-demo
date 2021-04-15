20446ece0aded5eee208bb192fb5bcbb1b7d05c9
Dulatto <dulat.omarov@gmail.com>
2021-04-14 23:37:10 -0400

    clean app

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,6 @@
-import {createStore} from 'redux'
+import { createStore } from 'redux'
 import cakeReducer from './cake/cakeReducer'
 
-
 const store = createStore(cakeReducer)
-
 
 export default store
