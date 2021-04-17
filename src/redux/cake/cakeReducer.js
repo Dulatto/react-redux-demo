3957ccd90c227ab13a6381e91ae7532f4cb5e96c
Dulatto <dulat.omarov@gmail.com>
2021-04-16 23:43:22 -0400

    clean app

--- src/redux/cake/cakeReducer.js
+++ src/redux/cake/cakeReducer.js
@@ -1,16 +1,16 @@
-import {BUY_CAKE} from './cakeTypes'
+import { BUY_CAKE } from './cakeTypes'
 
 const initialState = {
     numOfCakes: 10
 }
 
 const cakeReducer = (state = initialState, action) => {
-switch (action.type) {
-    case BUY_CAKE: return{
-        ...state,
-        numOfCakes: state.numOfCakes - 1
-    }      
-   default: return state      
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        default: return state
+    }
 }
-}
-export default cakeReducer
+export default cakeReducer;
